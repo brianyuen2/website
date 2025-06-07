04189070e4e67a5c4a93f24824044923691f90ee
Brian <brianyuen2@gmail.com>
2025-06-07 12:05:49 +1000

    Add title and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import {HeaderGallery} from "./components/header-gallery";
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+    title: 'Brian | Engineer & Creator',
+    icons: {
+        icon: '/assets/icon.png'
+    }
+}
+
 
 export default function Home() {
   return (
     <div>
-      <main >
+        <main >
         <HeaderGallery/>
       </main>
     </div>
